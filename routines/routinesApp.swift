3c04800b78ee7a1631cc79a4ac6665c3f1b9758a
hrishisd <hsdharam@gmail.com>
2024-11-23 13:38:19 -0500

    initial commit

---
+++ routines/routinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  routinesApp.swift
+//  routines
+//
+//  Created by Hrishikesh Dharam on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct routinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
